2c3d28fe42d097958405b50b76e67f68d78a0595
dimych <kuzyuberdin@gmail.com>
2025-12-01 23:15:37 +0400

    refactor styling

--- src/dal/api.ts
+++ src/dal/api.ts
@@ -3,14 +3,12 @@ const headers = {
     'api-key': apiKey
 }
 
-
 export const getTrack = (trackId: string) => {
     return fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks/' + trackId, {
         headers: headers
     })
         .then(res => res.json())
 }
-
 
 export const getTracks = () => {
     return fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks?pageSize=5', {
